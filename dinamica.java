d48778a17f980e48f593862440d13dd2097f3c4e
ichhito <ichirohitomicolegio@gmail.com>
2025-11-20 12:30:43 +0100

    feat: comienzo algoritmo dinamico

---
+++ dinamica.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections; 
+
+public class dinamica {
+
+    private int k;
+    private int n;
+    private ArrayList<Contenedor> contenedores; 
+
+    /**
+     * Constructor
+     */
+    public dinamica(int k, int n, ArrayList<Contenedor> contenedores) {
+        this.k = k; // Numero de contenedores
+        this.n = n; // Numero de atributos
+        this.contenedores = contenedores;   // Lista de contenedores
+    }
+
+    /**
+     * Metodo para lanzar el algoritmo
+     */
+    public ArrayList<Contenedor> empezar() {
+
+        return new ArrayList<Contenedor>();
+    }
+
+
+
+
+}
